119870c80bbbcb7e5d6c2ac1580b5045ae0d078c
leandro.iglezias <leeiglezias@gmail.com>
2025-08-31 20:01:08 -0300

    Initial

---
+++ okhttp-request/src/main/java/GetCep.java
@@ -0,0 +1,25 @@
+
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+
+
+public class GetCep {
+
+    final OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        GetCep example = new GetCep();
+        String response = example.run("https://raw.github.com/square/okhttp/master/README.md");
+        System.out.println(response);
+    }
+}
