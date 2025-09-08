e6fd589079dbb8272a24a8b00c0fe5b09f1b4c9d
leandro.iglezias <leeiglezias@gmail.com>
2025-09-07 21:14:34 -0300

    POC okHttp

--- okhttp-request/src/main/java/GetCep.java
+++ okhttp-request/src/main/java/GetCep.java
@@ -1,13 +1,13 @@
 
 import java.io.IOException;
-import okhttp3.OkHttpClient;
 
+import okhttp3.*;
 
 public class GetCep {
 
     final OkHttpClient client = new OkHttpClient();
 
-    String run(String url) throws IOException {
+    String runSync(String url) throws IOException {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
@@ -17,9 +17,34 @@ public class GetCep {
         }
     }
 
+    void runAsync(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                    }
+                    System.out.println(responseBody.string());
+                }
+            }
+        });
+    }
     public static void main(String[] args) throws IOException {
         GetCep example = new GetCep();
-        String response = example.run("https://raw.github.com/square/okhttp/master/README.md");
-        System.out.println(response);
+//        String response = example.runSync("https://viacep.com.br/ws/90550052/json/");
+//        System.out.println(response);
+        example.runAsync("https://viacep.com.br/ws/90550052/json/");
     }
 }
